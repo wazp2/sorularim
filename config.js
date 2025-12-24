61e30cb7c133d9138d15fea1b1c1ebef38df2dc5
Mehmet <61597302+wazp2@users.noreply.github.com>
2025-12-25 00:14:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,5 @@
 // config.js
 
-// 🔐 Admin olacak Gmail adresin
-export const ADMIN_EMAIL = "harplot28@gmail.com";
 
-// 🔥 Firebase Web App Config (Console'dan aldığın)
-export const firebaseConfig = {
- apiKey: "AIzaSyC8S7wbfbmrUJqgXia0rAQpy43ZMssYOs4",
-apiKey: "AIzaSyC8S7wbfbmrUJqgXia0rAQpy43ZMssYOs4",
-  authDomain: "bank-a3dff.firebaseapp.com",
-  projectId: "bank-a3dff",
-  storageBucket: "bank-a3dff.firebasestorage.app",
-  messagingSenderId: "755245660866",
-  appId: "1:755245660866:web:b185df3e6397a01cb8195b",
-  measurementId: "G-YCYDQ23PWZ"
+
 };
